0e2adb61408403397ae6337d7af944ae9f31b860
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2021-12-08 21:14:01 +0100

    menu glowne

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+document.querySelector('#app').innerHTML =  `  
+<div class="container">
+    <div class="item" id="game"><button class="button"><img class="img" src='resource/18966195.jpg'> <p class="menudescript"> Gra sorter </p></button></div> 
+    <div class="item" id="signs"><button class="button"><img class="img" src="resource/pet.jpg"><p class="menudescript">Oznaczenia na opakowaniach</p></button></div>
+    <div class="item" id="quiz"><button class="button"><img class="img" src="resource/49455.jpg"><p class="menudescript">Quiz </p></button></div>
+    <div class="item" id="air"><button class="button"><img class="img" src="resource/41284.jpg"><p class="menudescript">Jakość powietrza</p> </button></div>
+    <div class="item" id="base"><button class="button"><img class="img" src="resource/2451014.jpg"> <p class="menudescript">Baza wiedzy</p></button></div>
+    <div class="item" id="ranking"><button class="button"><img class="img" src="resource/puchar.png"><p class="menudescript">Ranking</p></button></div>
+</div>`;
+
