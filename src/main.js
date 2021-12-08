fc7be7423f7525ba29bf12561be50c902ef98151
DariuszKny <nasz_email_do_github>
2021-12-08 23:31:58 +0100

    dodano brakujące wywołanie funkcji showMainMenu przy starcie aplikacji

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './style.css';
 import { showMainMenu } from './mainMenu';
 
+showMainMenu()
+
 const mainMenuButton = document.getElementById('mainMenuButton');
 
 mainMenuButton.addEventListener('click', () => {
