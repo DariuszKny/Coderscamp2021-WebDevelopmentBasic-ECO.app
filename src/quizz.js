538e9131a31aeb846652c1a241d3a264dd6fae80
DariuszKny <nasz_email_do_github>
2021-12-08 23:22:24 +0100

    dodano przykładowy możliwy układ modułów w aplikacji

---
+++ src/quizz.js
@@ -0,0 +1,7 @@
+export function  showQuizMenu() {
+        document.querySelector('#app').innerHTML = `  
+        <div class="container">
+            <h1>TUTAJ JEST KONTENER GRY QUIZ</h1>
+        </div>`;
+    }
+
---
+++ src/sorter.js
@@ -0,0 +1,6 @@
+export function  showSorterMenu() {
+    document.querySelector('#app').innerHTML = `  
+    <div class="container">
+        <h1>TUTAJ JEST KONTENER DO GRY SORTER</h1>
+    </div>`;
+}
