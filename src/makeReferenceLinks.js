73a9860b5c911cdaffdfb1c015a98f7e0c648124
Agnieszka Kapelanczyk <agnieszka.kapelanczyk.ak@gmail.com>
2022-01-03 23:36:15 +0100

    zaakceptowanie poprawek

--- src/makeReferenceLinks.js
+++ src/makeReferenceLinks.js
@@ -11,8 +11,4 @@ export function makeReferenceLinks(hrefElement){
       containerWithReferenceLinks.appendChild(link);
     }
    
-<<<<<<< HEAD
   };
-=======
-  };
->>>>>>> bee3b3bacd1c08bd092e0de80d29135d0ab3dc7b
