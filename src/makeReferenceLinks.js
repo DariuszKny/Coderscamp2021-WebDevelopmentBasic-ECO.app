44f5eafada2eedad427e19cb83d325e0e6f94d17
Agnieszka Bury <ag.grala@gmail.com>
2022-01-04 13:01:58 +0100

    fix bug

--- src/makeReferenceLinks.js
+++ src/makeReferenceLinks.js
@@ -10,9 +10,4 @@ export function makeReferenceLinks(hrefElement){
     if (containerWithReferenceLinks) {
       containerWithReferenceLinks.appendChild(link);
     }
-   
-<<<<<<< HEAD
   };
-=======
-  };
->>>>>>> bee3b3bacd1c08bd092e0de80d29135d0ab3dc7b
